a0c508c2c3b99f608a924821c72d9090493ea268
SamiG42 <61887404+SamiG42@users.noreply.github.com>
2021-03-23 10:17:15 +0300

    Add files via upload

---
+++ YemekSeysi/YemekSeysi/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace YemekSeysi
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        public (int a, string b) Method1()
+        {
+            return (0, "");
+        }
+    }
+}
